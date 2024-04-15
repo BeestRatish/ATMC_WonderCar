c242ac2e0fa48635f9a3a2b1a81af500cab4fe65
BeestRatish <161386742+BeestRatish@users.noreply.github.com>
2024-04-14 22:25:03 -0700

    asas

--- defmove.py
+++ defmove.py
@@ -1,29 +1,20 @@
 #!/usr/bin/python3
 # coding=utf8
 import sys
-
-sys.path.append('/home/pi/TurboPi/')
 import cv2
 import time
 import math
 import signal
-import Camera
-import threading
 import numpy as np
 import yaml_handle
 import HiwonderSDK.Board as Board
 import HiwonderSDK.mecanum as mecanum
 import HiwonderSDK.FourInfrared as infrared
+import Camera
+import threading
 
-# Load other modules and define global variables as before...
-
-# Load the stop sign classifier
-stop_sign_cascade = cv2.CascadeClassifier('stop_sign_cascade.xml')
-
-# Add a global variable to track if the car should move or stop
-should_move = True
-
-# 红绿灯行驶
+# Define speed limit
+speed_limit_cascade = cv2.CascadeClassifier('speedlimit.xml')
 
 if sys.version_info.major == 2:
     print('Please run this program with python3!')
@@ -52,14 +43,14 @@ def load_config():
     servo_data = yaml_handle.get_yaml_data(yaml_handle.servo_file_path)
 
 
-# 初始位置
+# Initial Position
 def initMove():
     car.set_velocity(0, 90, 0)
     Board.setPWMServoPulse(1, servo1, 1000)
     Board.setPWMServoPulse(2, servo2, 1000)
 
 
-# 设置蜂鸣器
+# Set Buzzer
 def setBuzzer(timer):
     Board.setBuzzer(0)
     Board.setBuzzer(1)
@@ -78,20 +69,18 @@ range_rgb = {
 draw_color = range_rgb["black"]
 
 
-# 变量重置
+# Reset Variables
 def reset():
-    global car_stop
-    global color_list
-    global detect_color
-    global should_move
+    global car_stop, color_list, detect_color, servo1, servo2
 
     car_stop = False
     color_list = []
     detect_color = 'None'
-    should_move = True
+    servo1 = servo_data['servo1']
+    servo2 = servo_data['servo2']
 
 
-# app初始化调用
+# APP Initialization
 def init():
     print("LineFollower Init")
     load_config()
@@ -99,29 +88,27 @@ def init():
     initMove()
 
 
-# app开始玩法调用
+# App starts calling game program
 def start():
     global __isRunning
     reset()
     __isRunning = True
-    car.set_velocity(25, 90, 0)
+    car.set_velocity(20, 90, 0)
     print("LineFollower Start")
 
 
-# app停止玩法调用
+# App stops calling game program
 def stop():
-    global car_stop
-    global __isRunning
+    global car_stop, __isRunning
     car_stop = True
     __isRunning = False
     set_rgb('None')
     print("LineFollower Stop")
 
 
-# app退出玩法调用
+# Exit the game
 def exit():
-    global car_stop
-    global __isRunning
+    global car_stop, __isRunning
     car_stop = True
     __isRunning = False
     set_rgb('None')
@@ -135,7 +122,7 @@ def setTargetColor(color):
     return (True, ())
 
 
-# 设置扩展板的RGB灯颜色使其跟要追踪的颜色一致
+# Set RGB color on the expansion board consistent with the tracked color
 def set_rgb(color):
     if color == "red":
         Board.RGB.setPixelColor(0, Board.PixelColor(255, 0, 0))
@@ -155,116 +142,177 @@ def set_rgb(color):
         Board.RGB.show()
 
 
-# 找出面积最大的轮廓
-# 参数为要比较的轮廓的列表
+# Find the maximum contour
 def getAreaMaxContour(contours):
     contour_area_temp = 0
     contour_area_max = 0
     area_max_contour = None
 
-    for c in contours:  # 历遍所有轮廓
-        contour_area_temp = math.fabs(cv2.contourArea(c))  # 计算轮廓面积
+    for c in contours:
+        contour_area_temp = math.fabs(cv2.contourArea(c))
         if contour_area_temp > contour_area_max:
             contour_area_max = contour_area_temp
-            if contour_area_temp > 300:  # 只有在面积大于300时，最大面积的轮廓才是有效的，以过滤干扰
+            if contour_area_temp > 300:
                 area_max_contour = c
 
-    return area_max_contour, contour_area_max  # 返回最大的轮廓
+    return area_max_contour, contour_area_max
 
 
 def move():
-    global car_stop
-    global __isRunning
-    global detect_color
-    global should_move
+    global car_stop, __isRunning, detect_color
 
     while True:
         if __isRunning:
-            if should_move:  # Move the car if should_move is True
-                if detect_color != 'red':
-                    set_rgb(detect_color)  # Set the RGB color based on the detected color
-                    sensor_data = line.readData()  # Read data from 4 infrared sensors
-                    # Add your existing car movement logic here
-                    pass
-            else:  # Stop the car if should_move is False
-                car.set_velocity(0, 90, 0)  # Stop the car
-                time.sleep(0.01)  # Add a small delay to reduce CPU usage
+            if detect_color != 'red':
+                set_rgb(detect_color)
+                sensor_data = line.readData()
+                if not sensor_data[0] and sensor_data[1] and sensor_data[2] and not sensor_data[3]:
+                    car.set_velocity(35, 90, 0)
+                    car_stop = True
+                elif not sensor_data[0] and not sensor_data[1] and sensor_data[2] and not sensor_data[3]:
+                    car.set_velocity(35, 90, 0.03)
+                    car_stop = True
+                elif not sensor_data[0] and sensor_data[1] and not sensor_data[2] and not sensor_data[3]:
+                    car.set_velocity(35, 90, -0.03)
+                    car_stop = True
+                elif not sensor_data[0] and not sensor_data[1] and not sensor_data[2] and sensor_data[3]:
+                    car.set_velocity(35, 90, 0.3)
+                    car_stop = True
+                elif sensor_data[0] and not sensor_data[1] and not sensor_data[2] and not sensor_data[3]:
+                    car.set_velocity(35, 90, -0.3)
+                    car_stop = True
+
+                elif sensor_data[0] and sensor_data[1] and sensor_data[2] and sensor_data[3]:
+                    if car_stop:
+                        car.set_velocity(0, 90, 0)
+                        car_stop = False
+                    time.sleep(0.01)
+
+                if detect_color == 'green':
+                    if not car_stop:
+                        car.set_velocity(35, 90, 0)
+                        car_stop = True
+
+            else:
+                if car_stop:
+                    setBuzzer(0.1)
+                    set_rgb(detect_color)
+                    car.set_velocity(0, 90, 0)
+                    car_stop = False
+                time.sleep(0.01)
 
         else:
             if car_stop:
-                car.set_velocity(0, 90, 0)  # Stop the car
+                car.set_velocity(0, 90, 0)
                 car_stop = False
             time.sleep(0.01)
 
 
-# 运行子线程
+# Run child thread
 th = threading.Thread(target=move)
 th.setDaemon(True)
 th.start()
 
 
-# 机器人图像处理
+# Image processing
 def run(img):
-    global __isRunning
-    global detect_color, draw_color, color_list
-    global should_move
+    global __isRunning, detect_color, color_list
 
-    if not __isRunning:  # Check if the game is running, return the original image if not
+    if not __isRunning:
         return img
 
     img_copy = img.copy()
     img_h, img_w = img.shape[:2]
 
-    # Perform color and stop sign detection here
-    # You can add more conditions based on your needs
-    # For simplicity, this code assumes stop signs are red rectangles
-    gray = cv2.cvtColor(img_copy, cv2.COLOR_BGR2GRAY)
-    stop_signs = stop_sign_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5)
+    frame_resize = cv2.resize(img_copy, size, interpolation=cv2.INTER_NEAREST)
+    frame_gb = cv2.GaussianBlur(frame_resize, (3, 3), 3)
+    frame_lab = cv2.cvtColor(frame_gb, cv2.COLOR_BGR2LAB)
 
-    if len(stop_signs) > 0:  # Stop sign detected
-        should_move = False
-        for (x, y, w, h) in stop_signs:
-            cv2.rectangle(img_copy, (x, y), (x + w, y + h), (0, 0, 255), 2)
-            detect_color = 'red'  # Assuming stop signs are red
-            draw_color = (0, 0, 255)  # Red color for drawing
-            car.set_velocity(0, 90, 0)  # Stop the car when a stop sign is detected
-            time.sleep(3)  # Wait for 3 seconds before resuming
-    else:  # No stop sign detected
-        should_move = True
-        detect_color = 'None'
-        draw_color = range_rgb["black"]
+    # Speed limit detection
+    gray = cv2.cvtColor(frame_resize, cv2.COLOR_BGR2GRAY)
+    speed_limit_signs = speed_limit_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
 
-    cv2.putText(img_copy, "Color: " + detect_color, (10, img_h - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.65, draw_color, 2)
+    if len(speed_limit_signs) > 0:
+        car.set_velocity(50, 90, 0)
+    else:
+        max_area = 0
+        color_area_max = None
+        areaMaxContour_max = 0
 
-    return img_copy
+        for i in target_color:
+            if i in lab_data:
+                frame_mask = cv2.inRange(frame_lab,
+                                         (lab_data[i]['min'][0], lab_data[i]['min'][1], lab_data[i]['min'][2]),
+                                         (lab_data[i]['max'][0], lab_data[i]['max'][1], lab_data[i]['max'][2]))
+
+                opened = cv2.morphologyEx(frame_mask, cv2.MORPH_OPEN, np.ones((3, 3), np.uint8))
+                closed = cv2.morphologyEx(opened, cv2.MORPH_CLOSE, np.ones((3, 3), np.uint8))
+                contours = cv2.findContours(closed, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)[-2]
+                areaMaxContour, area_max = getAreaMaxContour(contours)
+
+                if areaMaxContour is not None:
+                    if area_max > max_area:
+                        max_area = area_max
+                        color_area_max = i
+                        areaMaxContour_max = areaMaxContour
+
+        if max_area > 2500:
+            rect = cv2.minAreaRect(areaMaxContour_max)
+            box = np.int0(cv2.boxPoints(rect))
+            cv2.drawContours(img, [box], -1, range_rgb[color_area_max], 2)
+
+            if color_area_max == 'red':
+                color = 1
+            elif color_area_max == 'green':
+                color = 2
+            else:
+                color = 0
+            color_list.append(color)
+
+            if len(color_list) == 3:
+                color = np.mean(np.array(color_list))
+                color_list = []
+                if color == 1:
+                    detect_color = 'red'
+                elif color == 2:
+                    detect_color = 'green'
+                else:
+                    detect_color = 'None'
+
+    cv2.putText(img, "Color: " + detect_color, (10, img.shape[0] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.65, draw_color, 2)
+
+    return img
 
 
-# 关闭前处理
 def manualcar_stop(signum, frame):
     global __isRunning
 
-    print('关闭中...')
+    print('Shutting down...')
     __isRunning = False
-    car.set_velocity(0, 90, 0)  # 关闭所有电机
+    car.set_velocity(0, 90, 0)
 
 
 if __name__ == '__main__':
     init()
     start()
     camera = Camera.Camera()
-    camera.camera_open(correction=True)  # 开启畸变矫正，默认不开启
+    camera.camera_open(correction=True)
+
     signal.signal(signal.SIGINT, manualcar_stop)
+
     while __isRunning:
         img = camera.frame
         if img is not None:
             frame = img.copy()
-            Frame = run(frame)
-            frame_resize = cv2.resize(Frame, (320, 240))
-            cv2.imshow('frame', frame_resize)
+            frame_processed = run(frame)
+            frame_resized = cv2.resize(frame_processed, (320, 240))
+            cv2.imshow('frame', frame_resized)
             key = cv2.waitKey(1)
             if key == 27:
                 break
         else:
             time.sleep(0.01)
+
     camera.camera_close()
     cv2.destroyAllWindows()
+
